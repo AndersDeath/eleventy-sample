e32161c00f9116f0c7cc89784a0c8c4b22b58c27
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-02-27 12:45:38 +0400

    add syntax highlight plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPlugin(syntaxHighlight);
     return {
         dir: {
             input: "src",
