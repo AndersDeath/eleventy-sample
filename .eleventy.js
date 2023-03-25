d466237017827d6527880719989948e0fdde537b
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-03-25 11:56:06 +0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require('markdown-it')
+const markdownItAttrs = require('markdown-it-attrs')
+const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  }
+
+  const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+    eleventyConfig.setLibrary('md', markdownLib)
+
     return {
         dir: {
             input: "src",
