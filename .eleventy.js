abe021db444b59db3265f631e17216816e0ec05d
Vladimir Bolshakov <andersdeath@yandex.ru>
2023-03-25 11:47:17 +0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPassthroughCopy({ "src/images": "images" });
     return {
         dir: {
             input: "src",
